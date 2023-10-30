836739112eb4e56155739d09aea1bd5135c0c225
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2023-10-30 19:44:43 -0400

    Time: 10 ms (90.39%), Space: 13.6 MB (10.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        output = False
+        stack = []
+        for i in range(len(s)):
+            if len(s) % 2 != 0:
+                output = False
+                break
+
+            if s[i] == '(' or s[i] == '[' or s[i] == '{':
+                stack.append(s[i])
+
+            elif s[i] == ')' or s[i] == ']' or s[i] == '}':
+                if not stack:
+                    output = False
+                    break
+
+                popped_val = stack.pop()
+
+                if (popped_val == '(' and s[i] == ')') or (popped_val == '[' and s[i] == ']') or (popped_val == '{' and s[i] == '}'):
+                    output = True
+        
+                else:
+                    output = False
+                    break
+        if stack:
+            output = False
+        return output
