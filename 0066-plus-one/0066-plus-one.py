ed138891ef9834062847030ec02d47ac08e71677
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2023-11-01 22:11:26 -0400

    Time: 20 ms (19.98%), Space: 13.4 MB (12.06%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+
+        length = len(digits) - 1
+
+        while length >= 0:
+
+            if digits[length] >= 9:
+                digits[length] = 0
+                length = length - 1
+                
+            else:
+                digits[length] += 1
+                return digits
+                
+        return [1] + digits
+        
