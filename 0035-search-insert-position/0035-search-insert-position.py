1fd86051c862fe4c4ccd61775a401171d953bca1
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2023-11-01 21:18:10 -0400

    Time: 259 ms (5.6%), Space: 13.8 MB (80.4%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        output = 0
+
+        for i in range(len(nums)):
+
+            if nums[i] == target:
+                output = i
+                break
+            elif target not in nums and target > nums[i]:
+                output = i + 1
+            elif target not in nums and target < nums[i]:
+                output = i 
+                break
+
+        return output
+
+
+        
