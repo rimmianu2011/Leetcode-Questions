b649d9b55ab8f5b05e3a58c7a18dfb5df8d9e2f8
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2023-11-01 21:39:46 -0400

    Time: 21 ms (7.69%), Space: 13.4 MB (51.25%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -5,13 +5,10 @@ class Solution(object):
         :rtype: int
         """
         list_words = s.split()
-        # print(s)
 
         length = len(list_words)
-        # print(length)
 
         lastWord = list_words[length-1]
-        # print(len(lastWord))
 
         output = len(lastWord)
 
