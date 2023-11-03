91ce470f948815bfbf4f7cc85eebea6b10706ce9
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2023-11-02 23:47:43 -0400

    Time: 23 ms (37.02%), Space: 13.2 MB (29.36%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -8,10 +8,30 @@ class Solution(object):
         if x < 2:
             return x
 
-        check = 2
+        # check = 2
 
-        while check * check <= x:
-            check = check + 1
+        # while check * check <= x:
+        #     check = check + 1
 
-        return check - 1
+        # return check - 1
+
+
+        start = 0
+        end = x
+
+        while abs(start-end) != 1:
+
+            middle = (start + end) // 2
+
+            if middle * middle == x:
+                return middle
+            if middle * middle > x:
+                end = middle
+            else:
+                start = middle
+
+        return start
+
+
+
         
