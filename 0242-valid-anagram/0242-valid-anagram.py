fb05afcad1f1184aceb04141bf80406a22540593
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2024-09-16 14:46:41 -0400

    Time: 61 ms (8.26%), Space: 16.9 MB (91.57%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return Counter(s) == Counter(t)
+
+        # This is an O(n) complexity solution
+
+        if len(s) != len(t):
+            return False
+
+        count_s = {}
+        count_t = {}
+        for char in range(len(s)):
+            count_s[s[char]] = 1 + count_s.get(s[char], 0)
+            count_t[t[char]] = 1+ count_t.get(t[char], 0) 
+        for count in count_t:
+            if count_t[count] != count_s.get(count, 0):
+                return False
+
+        return True
+        
+        
