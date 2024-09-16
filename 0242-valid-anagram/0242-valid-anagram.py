4b5e2ebaf0aa74b5beae7a4fdca86538ca309e83
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2024-09-16 14:54:35 -0400

    Time: 46 ms (71.69%), Space: 16.9 MB (46.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,6 +2,8 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # return Counter(s) == Counter(t)
 
+        # return sorted(s) == sorted(t)
+
         # This is an O(n) complexity solution
 
         if len(s) != len(t):
@@ -9,10 +11,13 @@ class Solution:
 
         count_s = {}
         count_t = {}
+
         for char in range(len(s)):
             count_s[s[char]] = 1 + count_s.get(s[char], 0)
             count_t[t[char]] = 1+ count_t.get(t[char], 0) 
+
         for count in count_t:
+            # print(count)
             if count_t[count] != count_s.get(count, 0):
                 return False
 
