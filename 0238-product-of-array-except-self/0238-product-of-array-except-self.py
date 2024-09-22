8d6f89d5ca0e24bf1a3d17faa5d73f2c4dde1777
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2024-09-21 20:04:17 -0400

    Time: 274 ms (40.88%), Space: 25.8 MB (48.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,19 +4,14 @@ class Solution:
         result = [1] * len(nums)
         pre = 1
         post = 1
-        i = 1
         for i in range(len(nums)):
             result[i] = pre
             pre *= nums[i] 
         # print(result)
 
-        i = len(result)
-        result[i-1] *= post
-        j = i -2
-        while j >= 0 :
-            post *= nums[j+1]
-            result[j] *= post
-            j -= 1
+        for i in range(len(nums) -1, -1, -1):
+            result[i] *= post
+            post *= nums[i]
         # print(result)
 
         return result
