bab80d077fa15578b8754bab1dc0fab30122e17f
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2024-11-18 13:52:57 -0500

    Time: 51 ms (5.02%), Space: 18.6 MB (16.66%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -5,13 +5,10 @@ class Solution:
         out = 0
         for num in tokens:
             if num in operators:
-                
-                # if len(stack) >= 2:
-                    num2 = stack.pop()
-                    # num1 = stack.pop()
-                    out = int(eval(stack.pop() + num + num2))
-                    # print(out)
-                    stack.append(str(out))
+                num2 = stack.pop()
+                num1 = stack.pop()
+                out = int(eval(num1 + num + num2))
+                stack.append(str(out))
                 
             else:
                 stack.append(num)
