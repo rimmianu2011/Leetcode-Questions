49fbee6f7e3ed024afa7403b1760c9034eba3c6b
Anushka Yadav <32792948+rimmianu2011@users.noreply.github.com>
2024-11-25 16:23:49 -0500

    Time: 20 ms (8.67%), Space: 16.9 MB (85.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,38 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s_new = re.sub(r'[^a-zA-Z0-9\s]', '', s)
-        s_new = s_new.lower()
-        s_new = "".join(s_new.split())
-        s = list(s_new)
-        s_flipped = []
-        for i in s[::-1]:
-            s_flipped.append(i)
-        s_f = ''.join(s_flipped)
 
-        if s_f == s_new:
-            return True
-        else:
-            return False
-        print(s_f)
-        print(s_new)
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.checkS(s[l]):
+                l += 1
+            while r > l and not self.checkS(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l+1, r-1
+        return True
+        
+    def checkS(self, n):
+        return (ord('A') <= ord(n) <= ord('Z') or
+               ord('a') <= ord(n) <= ord('z') or
+               ord('0') <= ord(n) <= ord('9'))
+
+
+        # solution 1
+        # s_new = re.sub(r'[^a-zA-Z0-9\s]', '', s)
+        # s_new = s_new.lower()
+        # s_new = "".join(s_new.split())
+        # s = list(s_new)
+        # s_flipped = []
+        # for i in s[::-1]:
+        #     s_flipped.append(i)
+        # s_f = ''.join(s_flipped)
+
+        # if s_f == s_new:
+        #     return True
+        # else:
+        #     return False
 
         
